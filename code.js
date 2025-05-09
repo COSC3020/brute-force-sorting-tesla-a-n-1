31960bfbde678586f2f88638b981d6e64892a8c1
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-08 18:25:32 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,59 +1,76 @@
 function permutationSort(a) {
-    return 0;
+    let permutationCount = 0;
+
+    //first permutation
+    let currentPermutation = [...a];
+
+    while (true) {
+        permutationCount++;
+
+        if (isSorted(currentPermutation)) {
+            // Apply perm to origional list
+            for (let i = 0; i < a.length; i++) {
+                a[i] = currentPermutation[i];
+            }
+            return permutationCount;
+        }
+
+        // Lexicographically generate next perm
+        if (!genNextLexicoPerm(currentPermutation)) {
+            break; // because there's no more permutations
+        }
+    }
+
+    return permutationCount;
 }
-/*
-    permutationCount = 0
-
-    first permutation
-    currentPermutation = copy a
-
-    while true:
-        permutationCount++
-
-        check if current perm is sorted
-        if isSorted(currentPermutation):
-            Apply perm to origional list
-            for i from 0 to length(a)-1:
-                a[i] = currentPermutation[i]
-            return permutationCount
-
-        Lexicographically generate next perm
-        if !genNextLexicoPerm(currentPermutation):
-            break because there's no more permutations
-
-    return permutationCount
 
 function isSorted(a):
-    for i from 0 to length(a)-2:
-        if a[i] > a[i+1]:
-            return false
-    return true
+    for (let i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i+1]) {
+            return false;
+        }
+    }
 
-function genNextLexicoPerm(a):
-    1 Find largest k that a[k] < a[k + 1] 
-    k = -1
-    for i from 0 to length(a)-2:
-        if a[i] < a[i+1]:
-            k=i
+    return true;
+}
 
-    if no such index exists, this is the last permutation
-    if k == -1:
-        return false
+function genNextLexicoPerm(a) {
+    //1 Find largest k that a[k] < a[k + 1] 
+    let k = -1;
+    for (let i = 0; i < a.length - 1; i++) {
+        if (a[i] < a[i+1]) {
+            k = i;
+        }
+    }
 
-    2 find largest index i > k that a[k] < a[i]
-    l = k + 1
-    for i from k+1 to length(a)-1:
-        if a[k] < a[i]:
-            l = i
+   // if no such index exists, this is the last permutation
+    if (k == -1) {
+        return false;
+    }
 
-    3 swap a[k] with a[l]
-    swap a[k] and a[l]
+    // 2 find largest index i > k that a[k] < a[i]
+    let l = k + 1;
+    for (let i = k + 1; i < a.length; i++) {
+        if (a[k] < a[i]) {
+            l = i;
+        }
+    }
 
-    4 reverse sequence from a[k+1} to end 
-    reverse subarray a[k+1] to a[length(a)-1]
+    // 3 swap a[k] with a[l]
+    [a[k], a[l]] = [a[l], a[k]];
 
-    ret t
-*/
+    // 4 reverse sequence from a[k+1} to end 
+    let left = k + 1;
+    let right = a.length - 1;
+    while (left < right) {
+        [a[left], a[right]] = [a[right], a[left]];
+        left++;
+        right--;
+    }
+    
+    return true;
+}
+
 
 
 
