2d8db2e72a4645c13ed4129fae724ed4772ffeb8
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-09 20:37:30 -0600

    reinclude isSorted

--- code.js
+++ code.js
@@ -32,3 +32,12 @@ function permutationSort(a) {
     heapPermuteIterative(a.length);
     return count;
 }
+
+function isSorted(a) {
+    for (let i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
