41c7b6cc5ea294339af2e3fc0f41cf2855164539
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-09 20:31:33 -0600

    Refactored to use heap's algorithm
    Source: https://en.wikipedia.org/wiki/Heap%27s_algorithm

--- code.js
+++ code.js
@@ -1,84 +1,40 @@
 function permutationSort(a) {
-    // Special case for empty or single-element arrays
-    if (a.length <= 1) {
-        return 1; // Already sorted
-    }
-    
-    let permutationCount = 0;
+    let count = 0;
+    let found = false;
 
-    //first permutation
-    let currentPermutation = [...a];
-
-    while (true) {
-        permutationCount++;
-
-        if (isSorted(currentPermutation)) {
-            // Apply perm to origional list
-            for (let i = 0; i < a.length; i++) {
-                a[i] = currentPermutation[i];
+    function heapPermute(k) {
+        if (k === 1) {
+            count++;
+            if (isSorted(a)) {
+                found = true;
             }
-            return permutationCount;
+            return;
         }
 
-        // Lexicographically generate next perm
-        if (!genNextLexicoPerm(currentPermutation)) {
-            break; // because there's no more permutations
+        heapPermute(k - 1);
+        if (found) return;
+
+        for (let i = 0; i < k - 1; i++) {
+            if (k % 2 === 0) {
+                [a[i], a[k - 1]] = [a[k - 1], a[i]];
+            } else {
+                [a[0], a[k - 1]] = [a[k - 1], a[0]];
+            }
+
+            heapPermute(k - 1);
+            if (found) return;
         }
     }
 
-    return permutationCount;
+    heapPermute(a.length);
+    return count;
 }
 
 function isSorted(a) {
     for (let i = 0; i < a.length - 1; i++) {
-        if (a[i] > a[i+1]) {
+        if (a[i] > a[i + 1]) {
             return false;
         }
     }
-
     return true;
 }
-
-function genNextLexicoPerm(a) {
-    //1 Find largest k that a[k] < a[k + 1] 
-    let k = -1;
-    for (let i = 0; i < a.length - 1; i++) {
-        if (a[i] < a[i+1]) {
-            k = i;
-        }
-    }
-
-   // if no such index exists, this is the last permutation
-    if (k === -1) {
-        return false;
-    }
-
-    // 2 find largest index i > k that a[k] < a[i]
-    let l = k + 1;
-    for (let i = k + 1; i < a.length; i++) {
-        if (a[k] < a[i]) {
-            l = i;
-        }
-    }
-
-    // 3 swap a[k] with a[l]
-    [a[k], a[l]] = [a[l], a[k]];
-
-    // 4 reverse sequence from a[k+1} to end 
-    let left = k + 1;
-    let right = a.length - 1;
-    while (left < right) {
-        [a[left], a[right]] = [a[right], a[left]];
-        left++;
-        right--;
-    }
-    
-    return true;
-}
-
-//maybe this will help the tests
-module.exports = {
-    permutationSort
-};
-
-
