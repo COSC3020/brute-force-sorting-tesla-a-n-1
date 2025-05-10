d4914f3d9b2dba62bd6417b53b4d7607d33d36c6
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-09 20:49:30 -0600

    frankenstein versions using Claude

--- code.js
+++ code.js
@@ -1,23 +1,26 @@
 function permutationSort(a) {
-    let count = 0;
+    if (a.length <= 1) return 1;
+    if (isSorted(a)) return 1;
+    let count = 1; // already checked initial array
     let found = false;
 
     function heapPermuteIterative(n) {
         let c = Array(n).fill(0);
         let i = 0;
 
-        while (i < n) {
+        while (i < n && !found) {
             if (c[i] < i) {
+
+                if (i % 2 === 0) { //Swap elemnts
+                    [a[0], a[i]] = [a[i], a[0]];
+                } else {
+                    [a[c[i]], a[i]] = [a[i], a[c[i]]];
+                }
+                
                 count++;
                 if (isSorted(a)) {
                     found = true;
                     return;
-                }
-
-                if (i % 2 === 0) {
-                    [a[0], a[i]] = [a[i], a[0]];
-                } else {
-                    [a[c[i]], a[i]] = [a[i], a[c[i]]];
                 }
                 
                 c[i]++;
