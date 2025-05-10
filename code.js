1f5cb5c3d506cd17a24557934b91edef4c017a58
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-09 20:35:37 -0600

    Used iterative heap instead of recursive
    Using the help of GitHub error, I reformatted the code to be iterative instead of recursive

--- code.js
+++ code.js
@@ -2,39 +2,33 @@ function permutationSort(a) {
     let count = 0;
     let found = false;
 
-    function heapPermute(k) {
-        if (k === 1) {
-            count++;
-            if (isSorted(a)) {
-                found = true;
-            }
-            return;
-        }
+    function heapPermuteIterative(n) {
+        let c = Array(n).fill(0);
+        let i = 0;
 
-        heapPermute(k - 1);
-        if (found) return;
+        while (i < n) {
+            if (c[i] < i) {
+                count++;
+                if (isSorted(a)) {
+                    found = true;
+                    return;
+                }
 
-        for (let i = 0; i < k - 1; i++) {
-            if (k % 2 === 0) {
-                [a[i], a[k - 1]] = [a[k - 1], a[i]];
+                if (i % 2 === 0) {
+                    [a[0], a[i]] = [a[i], a[0]];
+                } else {
+                    [a[c[i]], a[i]] = [a[i], a[c[i]]];
+                }
+                
+                c[i]++;
+                i = 0;
             } else {
-                [a[0], a[k - 1]] = [a[k - 1], a[0]];
+                c[i] = 0;
+                i++;
             }
-
-            heapPermute(k - 1);
-            if (found) return;
         }
     }
 
-    heapPermute(a.length);
+    heapPermuteIterative(a.length);
     return count;
-}
-
-function isSorted(a) {
-    for (let i = 0; i < a.length - 1; i++) {
-        if (a[i] > a[i + 1]) {
-            return false;
-        }
-    }
-    return true;
 }
